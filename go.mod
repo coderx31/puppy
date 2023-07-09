fe0b5c5ae93b9332892dba521718cb3d1b308488
= <=>
2023-07-09 12:43:52 +0530

    dog func added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/coderx31/puppy
 
 go 1.20
+
+require github.com/coderx31/dog v0.0.0-20230709070502-c201b9356753 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "dog"
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
 }
